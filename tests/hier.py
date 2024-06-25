22ffb6929145a459253b961398768c3ec836a3c2
firefirer1983 <fyman.zhang@gmail.com>
2024-06-25 17:12:12 +0800

    [refact]update hier.py

--- tests/hier.py
+++ tests/hier.py
@@ -1,20 +1,23 @@
 from transitions.extensions import HierarchicalGraphMachine
-
+from argparse import ArgumentParser
 states = ['standing', 'walking', {'name': 'caffeinated', 'children':['dithering', 'running']}]
 transitions = [
   ['walk', 'standing', 'walking'],
   ['stop', 'walking', 'standing'],
-  ['drink', '*', 'caffeinated'],
-  ['walk', ['caffeinated', 'caffeinated_dithering'], 'caffeinated_running'],
-  ['relax', 'caffeinated', 'standing']
+  ['walk', 'caffeinated_dithering', 'caffeinated_running'],
+  ['relax', 'caffeinated_running', 'standing'],
+  ['drink', 'walking', 'caffeinated_dithering']
 ]
 
 machine = HierarchicalGraphMachine(states=states, transitions=transitions, initial='standing', ignore_invalid_triggers=True)
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument("format")
+    args = parser.parse_args()
     model = machine.model
-    model.get_graph().draw("hier.json", prog='dot')
+    model.get_graph().draw("hier", prog='dot', format=args.format)
 
 
 if __name__ == "__main__":
